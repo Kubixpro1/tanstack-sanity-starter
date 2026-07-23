39603fc749eaac5a9dcf459689020e7089f383e0
Kubixpro1 <142821997+Kubixpro1@users.noreply.github.com>
2026-07-23 15:39:41 +0200

    Create index.ts

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,5 @@
+import { businessInfo } from "./businessInfo";
+import { page } from "./page";
+import { galleryItem } from "./galleryItem";
+
+export const schemaTypes = [businessInfo, page, galleryItem];
